1d9ce890d2f76960a85f00503eca021f268c6459
hungrytech <xorals9448@gmail.com>
2021-05-03 15:35:37 +0900

    Add aop dependency and UserServiceLogHelper

--- src/main/java/com/neconico/neconico/service/users/DefaultPolicyUserService.java
+++ src/main/java/com/neconico/neconico/service/users/DefaultPolicyUserService.java
@@ -3,14 +3,12 @@ package com.neconico.neconico.service.users;
 import com.neconico.neconico.dto.users.*;
 import com.neconico.neconico.mapper.users.UserMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -26,8 +24,6 @@ public class DefaultPolicyUserService implements UserService{
         userJoinDto.setAccountPw(
                 passwordEncoder.encode(userJoinDto.getAccountPw())
         );
-        log.info("사용자 비밀번호 암호화");
-
         userMapper.insertUser(userJoinDto);
         return userJoinDto.getUserId();
     }
@@ -56,13 +52,11 @@ public class DefaultPolicyUserService implements UserService{
     @Transactional
     public void changeDropUserAuthority(String accountId) {
         userMapper.updateUserAuthority(accountId);
-        log.info("사용자 권한 변경 해당아이디:{} {} -> {}", accountId, "ROLE_USER", "ROLE_DROP");
     }
 
     @Override
     @Transactional
     public void changeUserInfo(UserInfoDto userInfoDto) {
         userMapper.updateUserInfo(userInfoDto);
-        log.info("사용자 정보 변경");
     }
 }
