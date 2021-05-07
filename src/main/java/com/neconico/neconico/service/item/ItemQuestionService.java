cd2750dd3e347171032fb662b9237436daf85928
DESKTOP-Q7SSSEG\dae48 <dae4805@naver.com>
2021-05-07 12:17:31 +0900

    Refactor Service

--- src/main/java/com/neconico/neconico/service/item/ItemQuestionService.java
+++ src/main/java/com/neconico/neconico/service/item/ItemQuestionService.java
@@ -16,38 +16,25 @@ public class ItemQuestionService {
 
     private final ItemQuestionMapper itemQuestionMapper;
 
-    List<ItemQuestionVo> getQuestion(Long itemId) {
+    @Transactional(readOnly = true)
+    public List<ItemQuestionVo> getQuestion(Long itemId) {
         return itemQuestionMapper.selectItemQuestionListByItemID(itemId);
     }
 
-    void createQuestion(ItemQuestionDto inputQuestion) {
+    public void createQuestion(ItemQuestionDto inputQuestion) {
         itemQuestionMapper.insertItemQuestion(inputQuestion);
     }
 
-    void createComment(ItemQuestionDto inputComment) {
+    public void createComment(ItemQuestionDto inputComment) {
         itemQuestionMapper.insertItemQuestionComment(inputComment);
     }
 
-    void modifyQuestion(Long questionId, String content) {
-        itemQuestionMapper.updateItemQuestion(questionId, content, "ITEM_QUESTION");
+    public void modifyQuestion(Long questionId, String content, String kind) {
+        itemQuestionMapper.updateItemQuestion(questionId, content, kind);
     }
 
-    void modifyComment(Long commentId, String content) {
-        itemQuestionMapper.updateItemQuestion(commentId, content, "QUESTION_COMMENT");
+    public void deleteQuestion(Long questionId, String kind) {
+        itemQuestionMapper.deleteItemQuestion(questionId, kind);
     }
-
-    void deleteQuestion(Long questionId) {
-        itemQuestionMapper.deleteItemQuestion(questionId, "ITEM_QUESTION");
-    }
-
-    void deleteComment(Long commentId) {
-        itemQuestionMapper.deleteItemQuestion(commentId, "QUESTION_COMMENT");
-    }
-
-    boolean checkUserNWriterId(Long userId, Long writerId){
-        if(userId == writerId) return true;
-        return false;
-    }
-
 
 }
