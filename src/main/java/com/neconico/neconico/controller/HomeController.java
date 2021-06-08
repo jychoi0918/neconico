458524b98914db29feee5fe50de7abd5fc8663f7
jychoi0918 <jychoi091808@gmail.com>
2021-06-08 21:09:53 +0900

    Add footer html files and Update HomeController

--- src/main/java/com/neconico/neconico/controller/HomeController.java
+++ src/main/java/com/neconico/neconico/controller/HomeController.java
@@ -4,13 +4,15 @@ import com.neconico.neconico.dto.admin.advertisement.AdvertReturnDto;
 import com.neconico.neconico.dto.item.SearchInfoDto;
 import com.neconico.neconico.dto.item.card.ItemCardSearchViewDto;
 import com.neconico.neconico.paging.Criteria;
+import com.neconico.neconico.paging.Pagination;
 import com.neconico.neconico.service.admin.advertisement.AdvertiseService;
-import com.neconico.neconico.service.category.CategoryService;
+import com.neconico.neconico.service.admin.notice.NoticeService;
 import com.neconico.neconico.service.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,6 +22,7 @@ public class HomeController {
 
     private final ItemService itemService;
     private final AdvertiseService advertiseService;
+    private final NoticeService noticeService;
 
     @GetMapping("/")
     public String main(Model model) {
@@ -30,5 +33,41 @@ public class HomeController {
         model.addAttribute("advertList", advertReturnDtoList);
         model.addAttribute("itemCardList", itemCardSearchViewDtoList);
         return "index";
+    }
+
+
+    //footer
+    @GetMapping("/notice")
+    public String noticeList(Model model, Criteria cri) {
+
+        model.addAttribute("notices", noticeService.selectPublicNotices(cri));
+        model.addAttribute("pageMaker", new Pagination(cri, noticeService.countAllNotices().intValue(), 5));
+
+        return "footer/notice_public";
+
+    }
+
+    @GetMapping("/notice/{noticeId}")
+    public String noticeView(@PathVariable Long noticeId, Model model) {
+
+        model.addAttribute("notice", noticeService.selectNoticeByNoticeId(noticeId));
+
+        return "footer/notice_public_detail";
+    }
+
+    @GetMapping("/adinquiry")
+    public String adInquiry() {
+        return "footer/ad_inquiry";
+    }
+
+    @GetMapping("/question")
+    public String question() {
+        return "footer/question";
+    }
+
+
+    @GetMapping("/policy")
+    public String operationPolicy() {
+        return "footer/operation_policy";
     }
 }
